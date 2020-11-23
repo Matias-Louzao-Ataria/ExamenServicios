da2d8a54c8262a25f6034be3116e8c7c5ad88e57
Matias-Louzao-Ataria <louzao.ataria.matias@gmail.com>
2020-11-23 17:50:55 +0100

    Add files via upload
    Examen

---
+++ LouzaoAtariaMatiasSERV1EV/LouzaoAtariaMatiasSERV1EV/Program.cs
@@ -0,0 +1,91 @@
+﻿//Matias Louzao Ataria 54505421K
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace LouzaoAtariaMatiasSERV1EV
+{
+    class Program
+    {
+        public static int NOnions = 0;
+        public static int NPotatoes = 0;
+        public static int NOmelette = 0;
+        public static bool running = true;//cocinando, mientras no haya 10 tortillas.
+        public static Object l = new object();
+
+        static void Main(string[] args)
+        {
+            Thread tPotato= new Thread(() => Ingredient("Onion", ref NOnions));
+            Thread tOnion= new Thread(() => Ingredient("Potato", ref NPotatoes));
+            Thread tOmelette= new Thread(Omelette);
+            tPotato.Start();
+            tOnion.Start();
+            tOmelette.Start();
+            tOmelette.Join();
+            Console.WriteLine("The End, Pon Wins. No onion rules!");
+            Console.ReadKey();
+        }
+
+        public static void Ingredient(string ingredient,ref int cont)
+        {
+            while (running)
+            {
+                lock (l)
+                {
+                    if (running)
+                    {
+                        if (NOmelette < 10)
+                        {
+                            cont++;
+                            if (cont >= 5)
+                            {
+                                lock (l)
+                                {
+                                    Monitor.Pulse(l);
+                                }
+                            }
+                            Console.WriteLine("Ingredient: {0,7} {1,3}",ingredient,cont);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Omelette()
+        {
+            while (running)
+            {
+                lock (l)
+                {
+                    if (running)
+                    {
+                        if (NOmelette < 10)
+                        {
+                            if (NOnions >= 5 && NPotatoes >= 5)
+                            {
+                                NOmelette++;
+                                NOnions -= 5;
+                                NPotatoes -= 5;
+                                Console.WriteLine("Omelet: {0,3}, Onion: {1,3}, Potato: {2,3}",NOmelette,NOnions,NPotatoes);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Waiting...");
+                                lock (l)
+                                {
+                                    Monitor.Wait(l);
+                                }
+                            }
+                        }
+                        else 
+                        {
+                            running = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LouzaoAtariaMatiasSERV1EV/LouzaoAtariaMatiasSERV1EVEJ2/Program.cs
@@ -0,0 +1,59 @@
+﻿//Matias Louzao Ataria 54505421K
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace LouzaoAtariaMatiasSERV1EVEJ2
+{
+    class Program
+    {
+        public static Random r = new Random();
+        public delegate void MyDelegate();
+        static void Main(string[] args)
+        {
+            ExceptionControl(RandomInfo);
+            FileInfo f = new FileInfo(Environment.GetEnvironmentVariable("SYSTEMROOT")+"\\");
+            Console.WriteLine("Archivos en {0}:", Environment.GetEnvironmentVariable("SYSTEMROOT"));
+            foreach (FileInfo file in f.Directory.GetFiles())
+            {
+                Console.WriteLine(file.Name);
+            }
+            //Array.ForEach(f.Directory.GetFiles(),Console.WriteLine);
+            Console.ReadKey();
+        }
+
+        public static void RandomInfo()
+        {
+            Process[] pSistem = Process.GetProcesses();
+            //Process p = Process.GetProcessById(4220);
+            Process p = Process.GetProcessById(r.Next(pSistem.Length));
+            string str = "";
+            str += string.Format("Process name: {0}\n\r",p.ProcessName);
+            foreach (ProcessModule module in p.Modules)
+            {
+                str += string.Format("{0}\r\n",module.ModuleName);
+            }
+            Console.WriteLine(str);
+            using (StreamWriter writer = new StreamWriter(Environment.GetEnvironmentVariable("USERPROFILE")+"\\examen.txt"))
+            {
+                writer.Write(str);
+            }
+        }
+
+        public static void ExceptionControl(MyDelegate deleg)
+        {
+            try
+            {
+                deleg();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Panic Error!!");
+            }
+        }
+    }
+}
